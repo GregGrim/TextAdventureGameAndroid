6b0ebcd1315deb69e95f5de193d295585ba2570c
GrishaCoder <grimy.grigory@gmail.com>
2024-01-12 22:53:45 +0000

    added start menu

---
+++ app/src/main/java/com/example/textadventuregame/view/loadGameActivity.java
@@ -0,0 +1,16 @@
+package com.example.textadventuregame.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.textadventuregame.R;
+
+public class loadGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_game);
+    }
+}
